00e2b902ac0843d07c38a301bacf0b34bf1da44c
NGelezinis <ngelez2@uic.edu>
2021-07-21 16:08:21 -0500

    more fixes to app.module

--- p2fe/src/app/app.module.ts
+++ p2fe/src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -24,16 +22,10 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
     NgbModule
-=======
-    NgbModule,
-    HttpClientModule,
-    FormsModule
->>>>>>> 2490586efade6a294d2b3967cc6939a919eb7fa9
   ],
   providers: [],
   bootstrap: [AppComponent]
