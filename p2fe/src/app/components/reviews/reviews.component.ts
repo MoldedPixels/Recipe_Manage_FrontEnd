3cf4d53930d10e68295c04ab74ab0f134c49c99e
NGelezinis <ngelez2@uic.edu>
2021-07-29 16:04:52 -0500

    progress, now I need to deal with only 1 review being sent back, decide how to improve the layout and maybe testing with jest/helping my teammates

--- p2fe/src/app/components/reviews/reviews.component.ts
+++ p2fe/src/app/components/reviews/reviews.component.ts
@@ -1,6 +1,7 @@
 import { FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { ReviewByIdService } from '../../services/review-by-id.service';
+import { Review } from '../../models/review';
 
 @Component({
   selector: 'app-reviews',
@@ -12,12 +13,15 @@ export class ReviewsComponent implements OnInit {
   constructor(
     private reviewService: ReviewByIdService,
     private formBuilder: FormBuilder
-  ) { }
+  ) { 
+  }
 
   reviewForm =  this.formBuilder.group({
     idNum: ['', Validators.required],
     type: ['', Validators.required]
   });
+  results = 0;
+  reviews: Review[]=[];
 
   ngOnInit(): void {
   }
@@ -31,16 +35,22 @@ export class ReviewsComponent implements OnInit {
       if (this.reviewForm.invalid) return;
       this.reviewService.getReviewsById(this.reviewForm.controls['idNum'].value).subscribe((data) => {
         console.log(data);
+        this.reviews = data as Review[];
+        this.results = 1;
       });
     } else if(this.reviewForm.controls['type'].value == 'User'){
       if (this.reviewForm.invalid) return;
       this.reviewService.getReviewsByUser(this.reviewForm.controls['idNum'].value).subscribe((data) => {
         console.log(data);
+        this.reviews = data as Review[];
+        this.results = 1;
       });
     } else if(this.reviewForm.controls['type'].value == 'Recipe'){
       if (this.reviewForm.invalid) return;
       this.reviewService.getReviewsByRecipe(this.reviewForm.controls['idNum'].value).subscribe((data) => {
         console.log(data);
+        this.reviews = data as Review[];
+        this.results = 1;
       });
     } else {
       console.warn("you reached the else statement");
